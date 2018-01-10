30a45e379531bc6548a62fd2545304449321b4c0
igorsowa9 <igorsowa9@gmail.com>
2018-01-10 18:07:15 +0100

    rtds works

--- pyVM/settings.py
+++ pyVM/settings.py
@@ -1,15 +1,7 @@
 import platform, sys
 
-dbname = 'SAU_FC_pc'
-
-#if str(platform.machine())=='x86_64':
-#    IPinput = input("input your IP (leave empty for previous one): ")
-#    if IPinput == "":
-#        IPinput = '137.226.124.77'
-#else: IPinput = '134.130.169.61' # for raspberry
-
 IP_send = '134.130.169.96' # RTDS
-IP_receive = '137.226.133.186' # VM/RPI
+IP_receive = '134.130.169.12' # VM/RPI
 Port_send = 12345
 Port_receive = 12345
 
